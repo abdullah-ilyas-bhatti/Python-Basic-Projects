f8928ad60ec762578b8b6ac04b97dc6d59112118
abdullah-ilyas-bhatti <ab2977965@gmail.com>
2026-05-24 23:03:10 +0500

    Create  Secret_Auction_Program2.py

---
+++ Secret_Auction_Program2.py
@@ -0,0 +1,22 @@
+def find_high_bid(bidding_dictionary):
+
+    winner=max(bidding_dictionary, key=bidding_dictionary.get)
+    highest_amount=bidding_dictionary[winner]
+    print(f"the winner is {winner} with a bid is Rs{highest_amount} ")
+
+
+
+bid={}
+continue_bidding=True
+
+while continue_bidding:
+    name=input("Enter your Name: ")
+    price=int(input("Enter your Bid Price Rs"))
+    bid[name]=price
+
+    again=input("are there any bidder? Type yes or no ")
+    if again == "no":
+        continue_bidding=False
+        find_high_bid(bid)
+    elif again == "yes":
+        print("\n"*50)
