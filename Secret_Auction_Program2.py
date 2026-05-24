272dcfe71acde3c5bfe99dca5db7239210dbfbb7
abdullah-ilyas-bhatti <ab2977965@gmail.com>
2026-05-24 23:04:10 +0500

    Update Secret_Auction_Program2.py
    Secret Auction Program  |  using Max Function

--- Secret_Auction_Program2.py
+++ Secret_Auction_Program2.py
@@ -13,7 +13,6 @@ while continue_bidding:
     name=input("Enter your Name: ")
     price=int(input("Enter your Bid Price Rs"))
     bid[name]=price
-
     again=input("are there any bidder? Type yes or no ")
     if again == "no":
         continue_bidding=False
