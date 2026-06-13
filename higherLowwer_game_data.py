ebf6b4663e7052bfd4f8013b98182410be960331
Muhammad Abdullah Ilyas <ab2977965@gmail.com>
2026-06-14 00:29:10 +0500

    Create higherLowwer_game_data.py
    Just copy this data and paste into the previous file (higher lower game project) actual game , just do it and play and enjoy the game.

---
+++ higherLowwer_game_data.py
@@ -0,0 +1,70 @@
+logo = r"""
+  _   _ _       _               
+ | | | (_) __ _| |__   ___ _ __ 
+ | |_| | |/ _` | '_ \ / _ \ '__|
+ |  _  | | (_| | | | |  __/ |   
+ |_| |_|_|\__, |_| |_|\___|_|   
+ | |    __|___/    _____ _ __   
+ | |   / _ \ \ /\ / / _ \ '__|  
+ | |__| (_) \ V  V /  __/ |     
+ |_____\___/ \_/\_/ \___|_|     
+"""
+
+vs = r"""
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+"""
+
+data = [
+    {
+        'name': 'Instagram',
+        'followers': 667, 
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'followers': 632, 
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Lionel Messi',
+        'followers': 502,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Selena Gomez',
+        'followers': 428, 
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'followers': 400, 
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'The Rock',
+        'followes': 397, 
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ariana Grande',
+        'followers': 380, 
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'followers': 364,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    }
+]
